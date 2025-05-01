ed0e07a23f4a52abc2785bce26a8297d7415c216
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-05-01 01:04:19 -0300

    fix: Adjust routes with react-router-dom Link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className=" bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
                 <h1 className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SmartMark Solutions</h1>
@@ -9,19 +11,19 @@ const Navbar = () => {
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar">
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium md:space-x-8 md:flex-row md:mt-0 ">
                             <li>
-                                <a href="#" className="block py-2 px-3 text-white md:hover:text-blue-700" aria-current="page">Cadastro de produtos</a>
+                                <Link to="/" className="block py-2 px-3 text-white md:hover:text-blue-700">Produtos</Link>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-3 text-white md:hover:text-blue-700" >Categorias</a>
+                                <Link to="/register-products" className="block py-2 px-3 text-white md:hover:text-blue-700" aria-current="page">Cadastro de produtos</Link>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-3 text-white md:hover:text-blue-700">Produtos</a>
+                                <Link to="#" className="block py-2 px-3 text-white md:hover:text-blue-700" >Categorias</Link>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-3 text-white md:hover:text-blue-700">Vendas</a>
+                                <Link to="#" className="block py-2 px-3 text-white md:hover:text-blue-700">Vendas</Link>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 px-3 text-white md:hover:text-blue-700">Gráficos</a>
+                                <Link to="#" className="block py-2 px-3 text-white md:hover:text-blue-700">Gráficos</Link>
                             </li>
                         </ul>
                     </div>
